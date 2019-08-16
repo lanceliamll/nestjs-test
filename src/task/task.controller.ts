76a331f445f672603a2c132f3144bc01286a2ac9
lanceliamll <lainaliam5599@gmail.com>
2019-08-17 01:51:40 +0800

    Added Functionalities: GetTaskById, DeleteTaskById

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,15 +1,21 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task } from './task.model';
 import { CreateTaskDTO } from './dto/create-task.dto';
 
-@Controller('task')
+@Controller('task') //Represents localhost:3000/task
 export class TaskController {
   constructor(private taskService: TaskService) { }
+
 
   @Get()
   getAllTasks(): Task[] {
     return this.taskService.getAllTasks();
+  }
+
+  @Get("/:id")
+  getTaskById(@Param("id") id: string): Task {
+    return this.taskService.getTaskById(id);
   }
 
   //@Body means kinukuha nya yung key doon sa body, so that hindi malilito sa pag mamap si data;
@@ -17,4 +23,11 @@ export class TaskController {
   createTask(@Body() createTaskDto: CreateTaskDTO): Task {
     return this.taskService.createTask(createTaskDto);
   }
+
+  @Delete("/:id")
+  deleteTaskById(@Param("id") id: string): void {
+    return this.taskService.deleteTaskById(id);
+  }
+
+
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -7,10 +7,17 @@ import { CreateTaskDTO } from './dto/create-task.dto';
 export class TaskService {
   private tasks: Task[] = [];
 
+
+  // GET all the task in the array
   getAllTasks(): Task[] {
     return this.tasks;
   }
 
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  //CREATE a task to the array, createTaskDto means the shape of the model/data
   createTask(createTaskDto: CreateTaskDTO): Task {
     const { title, description } = createTaskDto;
     const task: Task = {
@@ -22,5 +29,20 @@ export class TaskService {
 
     this.tasks.push(task);
     return task;
+  }
+
+  //DELETE a task
+  deleteTaskById(id: string): void {
+    //#First Solution
+    // let index = this.tasks.map(task => task.id).indexOf(id);
+    // this.tasks.splice(index, 1);
+    // return this.tasks;
+
+    //#Second Solution
+    // let index = this.tasks.filter(task => task.id != id);
+    // return index;
+
+    //#Third Solution
+    this.tasks = this.tasks.filter(task => task.id !== id);
   }
 }
